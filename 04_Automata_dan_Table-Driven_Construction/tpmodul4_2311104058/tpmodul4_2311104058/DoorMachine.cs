b723985ef2a1c5253eeaa6eaf228ffbe6a83c442
Dimastian Aji Wibowo <147970502+dimastianaji@users.noreply.github.com>
2025-03-04 15:00:33 +0700

    Add files via upload

---
+++ 04_Automata_dan_Table-Driven_Construction/tpmodul4_2311104058/tpmodul4_2311104058/DoorMachine.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_2311104058
+{
+    public enum DoorState
+    {
+        Terkunci,
+        Terbuka
+    }
+
+    public class DoorMachine
+    {
+        private DoorState _state;
+
+        public DoorMachine()
+        {
+            _state = DoorState.Terkunci;
+            Console.WriteLine("Pintu terkunci");
+        }
+
+        public void BukaPintu()
+        {
+            if (_state == DoorState.Terkunci)
+            {
+                _state = DoorState.Terbuka;
+                Console.WriteLine("Pintu tidak terkunci");
+            }
+            else
+            {
+                Console.WriteLine("Pintu sudah terbuka");
+            }
+        }
+
+        public void KunciPintu()
+        {
+            if (_state == DoorState.Terbuka)
+            {
+                _state = DoorState.Terkunci;
+                Console.WriteLine("Pintu terkunci");
+            }
+            else
+            {
+                Console.WriteLine("Pintu sudah terkunci");
+            }
+        }
+    }
+
+}
+
+

1a30b040ef91f1f0893b750cf4d053dd830a370a
Dimastian Aji Wibowo <147970502+dimastianaji@users.noreply.github.com>
2025-03-04 14:57:31 +0700

    Add files via upload

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_CSharp/jurnal/modul2_2311104058/modul2_2311104058/Program.cs
@@ -0,0 +1,17 @@
+﻿using modul2_2311104058;
+
+class Program
+{
+    static void Main()
+    {
+        Salam salam = new Salam();
+        salam.InputNama();
+        salam.TampilkanSalam();
+
+        ArrayProcessor arrayProcessor = new ArrayProcessor();
+        arrayProcessor.PrintArray();
+
+        PrimeChecker primeChecker = new PrimeChecker();
+        primeChecker.CheckPrime();
+    }
+}
